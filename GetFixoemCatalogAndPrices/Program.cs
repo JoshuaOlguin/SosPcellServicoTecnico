8216603cd04b91a1af5912a8600e6884e57616f5
JoshuaOlguin <joshua.e.olguin@gmail.com>
2026-08-12 18:59:01 -0600

    Complete the first version of application

--- GetFixoemCatalogAndPrices/Program.cs
+++ GetFixoemCatalogAndPrices/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GetFixoemCatalogAndPrices.Dto;
+using GetFixoemCatalogAndPrices.Service;
+using GetFixoemCatalogAndPrices.Utilities;
+using System;
+using System.Text.Json;
+
 
 namespace GetFixoemCatalogAndPrices
 {
@@ -10,7 +11,28 @@ namespace GetFixoemCatalogAndPrices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string apiKey = "7x7X2W9L3A";
+            int maxResults;
+
+            Console.WriteLine("Introduce el nombre del articulo que deseas buscar");
+            string searchParameter = Console.ReadLine();
+
+            var client = new ApiClientService();
+            
+            string apiResponse = client.GetCatalogBySearchParameter(apiKey, Common.HtmlEncodeStringValue(searchParameter)).GetAwaiter().GetResult();
+            JsonDocument document = JsonDocument.Parse(apiResponse);
+            maxResults = document.RootElement.GetProperty("totalItems").GetInt32();
+
+            string result = client.GetCatalogBySearchParameter(apiKey, Common.HtmlEncodeStringValue(searchParameter), maxResults).GetAwaiter().GetResult();
+            var productResponse = Common.DeserializeResponse<ProductResponse>(result);
+
+            string filePath = @"C:\Users\quetz\OneDrive\Escritorio\" + searchParameter + ".xlsx";
+            Excel.CreateFile(filePath);
+
+            var items = Adapter.ConvertProductResponseItemsToItemList(productResponse);
+            Excel.PopulateExcelFile(filePath, items);
+
+            Console.WriteLine("Archivo Excel creado exitosamente en: " + filePath);
         }
     }
 }
